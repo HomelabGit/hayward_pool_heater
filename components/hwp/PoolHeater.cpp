9fcb074cc5bfe4918c8c9a18de6203517502c337
HomelabGit <jason.chapman@live.com.au>
2026-02-01 09:00:08 +1100

    Update PoolHeater.cpp

--- components/hwp/PoolHeater.cpp
+++ components/hwp/PoolHeater.cpp
@@ -113,7 +113,8 @@ void PoolHeater::update() {
     if(this->hp_data_.fan_mode.has_value() ) {
         
         //auto custom_fan_mode = this->hp_data_.fan_mode->to_custom_fan_mode();
-        this->custom_fan_mode_ = this->hp_data_.fan_mode->to_custom_fan_mode();
+        //this->custom_fan_mode_ = this->hp_data_.fan_mode->to_custom_fan_mode();
+        this->custom_fan_mode = this->hp_data_.fan_mode->to_custom_fan_mode().value();
 
        // this->set_custom_fan_mode(this->hp_data_.fan_mode->to_custom_fan_mode());
         this->custom_fan_mode_ = custom_fan_mode; 
